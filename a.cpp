1c230b8b1436758b37b6c7ef8c538ee072b54575
Bruno Monteiro <malettabruno@gmail.com>
2026-01-30 15:24:06 -0300

    Finishes opts parsing

--- a.cpp
+++ a.cpp
@@ -5,12 +5,16 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	tgen::registerGen(argc, argv);
+	tgen::register_gen(argc, argv);
 
 	int max_c = tgen::opt<int>("c");
+	int B = tgen::opt<bool>("b");
+	int f = tgen::opt<float>("f");
 
 	int a = tgen::rnd.next(0, max_c+1);
 	int b = tgen::rnd.next(0, max_c+1);
 
 	cout << a << " " << b << endl;
+	cout << B << endl;
+	cout << f << endl;
 }
--- src/tgen.h
+++ src/tgen.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <map>
 #include <random>
+#include <cctype>
 
 namespace tgen {
 
@@ -75,10 +76,27 @@ struct random {
 };
 
 /*
+ * Error handling.
+ */
+void __throw_assertion_error(const std::string& condition, const std::string& msg) {
+	throw std::runtime_error("tgen: " + msg + " (assertion `" + condition + "` failed).");
+}
+void __throw_assertion_error(const std::string& condition) {
+	throw std::runtime_error("tgen: assertion `" + condition + "` failed.");
+}
+std::runtime_error __error(const std::string& msg) {
+	return std::runtime_error("tgen: " + msg);
+}
+
+/*
+ * Ensures condition is true, with nice debug.
+ */
+#define ensure(cond, ...) if (!(cond)) __throw_assertion_error(#cond, __VA_ARGS__);
+
+/*
  * Global random generation.
  */
 random rnd;
-
 
 /*
  * Opts - options given to the generator.
@@ -120,12 +138,21 @@ bool has_opt(const std::string& key) {
 }
 
 /* INTERNAL
+ * Parses 'value' into bool.
+ */
+ bool __get_opt_bool(std::string value) {
+	if (value == "true" or value == "1") return true;
+	if (value == "false" or value == "0") return false;
+	throw __error("invalid value " + value + " for type bool");
+ }
+
+/* INTERNAL
  * Parses 'value' into integral type T.
  */
  template<typename T>
  T __get_opt_integral(std::string value) {
-	 // TODO
-	 return T();
+	 if (std::is_unsigned<T>()) return static_cast<T>(std::stoull(value));
+	 return static_cast<T>(std::stoll(value));
  }
 
 /* INTERNAL
@@ -133,8 +160,7 @@ bool has_opt(const std::string& key) {
  */
  template<typename T>
  T __get_opt_floating(std::string value) {
-	 // TODO
-	 return T();
+	 return static_cast<T>(std::stold(value));
  }
 
 /* INTERNAL
@@ -142,27 +168,20 @@ bool has_opt(const std::string& key) {
  */
  template<typename T>
  T __get_opt(std::string value) {
-	if (std::is_integral<T>()) return __get_opt_integral<T>(value);
-	if (std::is_floating_point<T>()) return __get_opt_floating<T>(value);
-	throw();
+	 try {
+		 if (std::is_same<T, bool>()) return __get_opt_bool(value);
+		 if (std::is_integral<T>()) return __get_opt_integral<T>(value);
+		 if (std::is_floating_point<T>()) return __get_opt_floating<T>(value);
+	 } catch (std::invalid_argument er) {}
+	 throw __error("invalid value " + value + " for type " + typeid(T).name());
  }
-
-/* INTERNAL
- * Parses 'value' into bool.
- */
- template<>
- bool __get_opt(std::string value) {
-	if (value == "true" or value == "1") return true;
-	if (value == "false" or value == "0") return false;
-	throw();
-}
 
 /*
  * Returns the parsed opt by a given index.
  */
 template<typename T>
 T opt(int index) {
-    if (!has_opt(key)) throw();
+	ensure(has_opt(index), "cannot find key with index " + index);
     return __get_opt<T>(__pos_opts[index]);
 }
 
@@ -181,7 +200,7 @@ T opt(int index, const T& default_value) {
  */
 template<typename T>
 T opt(const std::string& key) {
-    if (!has_opt(key)) throw();
+	ensure(has_opt(key), "cannot find key with key " + key);
     return __get_opt<T>(__named_opts[key]);
 }
 
@@ -196,17 +215,95 @@ T opt(const std::string& key, const T& default_value) {
 }
 
 /* INTERNAL
- * TODO
+ * Tries to fetch char from c string.
  */
-void __prepare_opts(int argc, char** argv) {
-	// TODO
+char __fetch_char(char* s, int idx) {
+	ensure(s[idx] != '\n', "tried to fetch end of string");
+	return s[idx];
+}
+
+/* INTERNAL
+ * Reads non-empty string until it hits a ' ' or the string ends.
+ */
+std::string __read_until(char* s) {
+	std::string read_str;
+	int idx = 0;
+	while (s[idx] != '\0') {
+		char nxt_char = __fetch_char(s, idx++);
+		if (nxt_char == ' ') break;
+		read_str += nxt_char;
+	}
+
+	ensure(read_str.size() > 0, "read string cannot be empty");
+	return read_str;
+}
+
+/* INTERNAL
+ * Parses the opts into __pos_opts vector and __named_opts map.
+ */
+void __parse_opts(int argc, char** argv) {
+	// Number of positional opts read.
+	int positional_cnt = 0;
+
+	// Starting from 1 to ignore the name of the executable.
+	for (int i = 1; i < argc; i++) {
+		std::string key = __read_until(argv[i]);
+
+		if (key[0] == '-') {
+			ensure(key.size() > 1, "invalid opt (" + std::string(argv[i])
+				+ ")");
+			if (isdigit(key[1])) {
+				// This case is a positional negative number argument
+				__pos_opts.push_back(key);
+				continue;
+			}
+
+			// pops first char '-'
+			key = key.substr(1);
+		} else {
+			// This case is a positional argument that does not start with '-'
+			__pos_opts.push_back(key);
+			continue;
+		}
+
+		// Pops a possible second char '-'.
+		if (key[0] == '-') {
+			ensure(key.size() > 1, "invalid opt (" + std::string(argv[i])
+				+ ")");
+
+			// pops first char '-'
+			key = key.substr(1);
+		}
+
+		// Assumes that, if it starts with '-' and second char is not a digit,
+		// then it is a <key, value> pair.
+		// 1 or 2 chars '-' have already been poped.
+
+		size_t eq = key.find('=');
+		if (eq != std::string::npos) {
+			// This is the '--key=value' case.
+			std::string value = key.substr(eq+1);
+			key = key.substr(0, eq);
+			ensure(key.size() > 0 and value.size() > 0, "expected non-empty\
+				key/value in opt (" + std::string(argv[1]));
+			ensure(__named_opts.count(key) == 0, "cannot have repeated keys");
+			__named_opts[key] = value;
+		} else {
+			// This is the '--key value' case.
+			ensure(__named_opts.count(key) == 0, "cannot have repeated keys");
+			ensure(argv[i+1], "value cannot be empty");
+			__named_opts[key] = __read_until(argv[i+1]);
+			i++;
+		}
+	}
 }
 
 /*
  * Registers generator by initializing rnd and parsing opts.
  * Argument 'version' is unused.
+ *
  */
-void register_gen(int argc, char** argv, int version = 1) {
+void register_gen(int argc, char** argv) {
 	std::vector<uint32_t> seed;
 
 	// Starting from 1 to ignore the name of the executable.
@@ -221,7 +318,7 @@ void register_gen(int argc, char** argv, int version = 1) {
 	}
 	rnd.seed(seed);
 
-	__prepar_oOpts(argc, argv);
+	__parse_opts(argc, argv);
 }
 
 }; // namespace tgen
