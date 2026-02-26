86fa45a7511d79566af147893cd2a959dce75c8a
Bruno Monteiro <malettabruno@gmail.com>
2026-02-26 19:55:33 -0300

    Adds github button

--- docs/html/search/all_7.js
+++ docs/html/search/all_7.js
@@ -7,8 +7,16 @@ var searchData=
   ['is_5fassociative_5fcontainer_5finternal_3c_20t_2c_20std_3a_3avoid_5ft_3c_20typename_20t_3a_3akey_5ftype_2c_20typename_20t_3a_3akey_5fcompare_20_3e_20_3e_4',['is_associative_container_internal&lt; T, std::void_t&lt; typename T::key_type, typename T::key_compare &gt; &gt;',['../structtgen_1_1is__associative__container__internal_3_01T_00_01std_1_1void__t_3_01typename_01T_1_3d354157a02e12a2c41e4e560faa1698.html',1,'tgen']]],
   ['is_5fcontainer_5finternal_5',['is_container_internal',['../structtgen_1_1is__container__internal.html',1,'tgen']]],
   ['is_5fcontainer_5finternal_3c_20std_3a_3astring_20_3e_6',['is_container_internal&lt; std::string &gt;',['../structtgen_1_1is__container__internal_3_01std_1_1string_01_4.html',1,'tgen']]],
-  ['is_5fcontainer_5finternal_3c_20t_2c_20std_3a_3avoid_5ft_3c_20decltype_28std_3a_3abegin_28std_3a_3adeclval_3c_20t_20_3e_28_29_29_29_2c_20decltype_28std_3a_3aend_28std_3a_3adeclval_3c_20t_20_3e_28_29_29_29_3e_20_3e_7',['is_container_internal&lt; T, std::void_t&lt; decltype(std::begin(std::declval&lt; T &gt;())), decltype(std::end(std::declval&lt; T &gt;()))&gt; &gt;',['../structtgen_1_1is__container__internal_3_01T_00_01std_1_1void__t_3_01decltype_07std_1_1begin_07stb858632f6bed9ee6114ba97895af7b49.html',1,'tgen']]],
-  ['is_5fprime_8',['is_prime',['../group__math.html#ga5ee9541bd1b0e242551eb4c626241f4d',1,'tgen::math']]],
-  ['is_5ftuple_5finternal_9',['is_tuple_internal',['../structtgen_1_1is__tuple__internal.html',1,'tgen']]],
-  ['is_5ftuple_5finternal_3c_20std_3a_3atuple_3c_20ts_2e_2e_2e_20_3e_20_3e_10',['is_tuple_internal&lt; std::tuple&lt; Ts... &gt; &gt;',['../structtgen_1_1is__tuple__internal_3_01std_1_1tuple_3_01Ts_8_8_8_01_4_01_4.html',1,'tgen']]]
+  ['is_5fcontainer_5finternal_3c_20t_2c_20std_3a_3avoid_5ft_3c_20typename_20t_3a_3avalue_5ftype_2c_20decltype_28std_3a_3abegin_28std_3a_3adeclval_3c_20t_20_3e_28_29_29_29_2c_20decltype_28std_3a_3aend_28std_3a_3adeclval_3c_20t_20_3e_28_29_29_29_3e_20_3e_7',['is_container_internal&lt; T, std::void_t&lt; typename T::value_type, decltype(std::begin(std::declval&lt; T &gt;())), decltype(std::end(std::declval&lt; T &gt;()))&gt; &gt;',['../structtgen_1_1is__container__internal_3_01T_00_01std_1_1void__t_3_01typename_01T_1_1value__type_528ff096c039829218d61cf6e967b1da.html',1,'tgen']]],
+  ['is_5fcontainer_5fmultiline_5finternal_8',['is_container_multiline_internal',['../structtgen_1_1is__container__multiline__internal.html',1,'tgen']]],
+  ['is_5fpair_5finternal_9',['is_pair_internal',['../structtgen_1_1is__pair__internal.html',1,'tgen']]],
+  ['is_5fpair_5finternal_3c_20std_3a_3apair_3c_20a_2c_20b_20_3e_20_3e_10',['is_pair_internal&lt; std::pair&lt; A, B &gt; &gt;',['../structtgen_1_1is__pair__internal_3_01std_1_1pair_3_01A_00_01B_01_4_01_4.html',1,'tgen']]],
+  ['is_5fpair_5fmultiline_5finternal_11',['is_pair_multiline_internal',['../structtgen_1_1is__pair__multiline__internal.html',1,'tgen']]],
+  ['is_5fpair_5fmultiline_5finternal_3c_20std_3a_3apair_3c_20a_2c_20b_20_3e_20_3e_12',['is_pair_multiline_internal&lt; std::pair&lt; A, B &gt; &gt;',['../structtgen_1_1is__pair__multiline__internal_3_01std_1_1pair_3_01A_00_01B_01_4_01_4.html',1,'tgen']]],
+  ['is_5fprime_13',['is_prime',['../group__math.html#ga5ee9541bd1b0e242551eb4c626241f4d',1,'tgen::math']]],
+  ['is_5fscalar_5finternal_14',['is_scalar_internal',['../structtgen_1_1is__scalar__internal.html',1,'tgen']]],
+  ['is_5ftuple_5finternal_15',['is_tuple_internal',['../structtgen_1_1is__tuple__internal.html',1,'tgen']]],
+  ['is_5ftuple_5finternal_3c_20std_3a_3atuple_3c_20ts_2e_2e_2e_20_3e_20_3e_16',['is_tuple_internal&lt; std::tuple&lt; Ts... &gt; &gt;',['../structtgen_1_1is__tuple__internal_3_01std_1_1tuple_3_01Ts_8_8_8_01_4_01_4.html',1,'tgen']]],
+  ['is_5ftuple_5fmultiline_5finternal_17',['is_tuple_multiline_internal',['../structtgen_1_1is__tuple__multiline__internal.html',1,'tgen']]],
+  ['is_5ftuple_5fmultiline_5finternal_3c_20std_3a_3atuple_3c_20ts_2e_2e_2e_20_3e_20_3e_18',['is_tuple_multiline_internal&lt; std::tuple&lt; Ts... &gt; &gt;',['../structtgen_1_1is__tuple__multiline__internal_3_01std_1_1tuple_3_01Ts_8_8_8_01_4_01_4.html',1,'tgen']]]
 ];
