3c32a038975eeb477a592a6f2c71839ce8565a08
Bruno Monteiro <malettabruno@gmail.com>
2026-02-26 22:01:00 -0300

    Add small logo do docs

--- docs/html/search/pages_9.js
+++ docs/html/search/pages_9.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['sequence_0',['sequence',['../md_docs_2sequence.html',1,'']]]
+  ['registering_0',['Registering',['../group__opts.html#autotoc_md60',1,'']]]
 ];
--- docs/html/search/pages_a.js
+++ docs/html/search/pages_a.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['tgen_0',['tgen',['../index.html',1,'']]],
-  ['type_20generators_20and_20instances_1',['Type generators and instances',['../index.html#autotoc_md2',1,'']]]
+  ['type_20generators_20and_20instances_0',['Type generators and instances',['../index.html#autotoc_md2',1,'']]]
 ];
